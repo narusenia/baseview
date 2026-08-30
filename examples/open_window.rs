0ffff247e008679e3e3e30a04107e14c37dba62b
Naruse Nia <notshinsin0000@gmail.com>
2026-08-31 01:16:15 +0900

    macos: stop turning off mouse coalescing for the whole host process

--- examples/open_window.rs
+++ examples/open_window.rs
@@ -29,7 +29,9 @@ impl WindowHandler for OpenWindowExample {
 
                 #[cfg(target_os = "macos")]
                 match e {
-                    MouseEvent::ButtonPressed { .. } => copy_to_clipboard(&"This is a test!"),
+                    MouseEvent::ButtonPressed { .. } => {
+                        copy_to_clipboard(&"This is a test!")
+                    }
                     _ => (),
                 }
             }
